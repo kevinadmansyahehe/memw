e4df65a4d1b1852d2a5094303ce0ba94429e67f1
unknown <dodorobe564@gmail.com>
2023-07-06 18:26:36 +0700

    Repair DB

--- model/Connect.go
+++ model/Connect.go
@@ -1,24 +1,23 @@
 package model
- 
+
 import (
 	"fmt"
- 
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
- 
+
 var DB *gorm.DB
- 
+
 func ConnectDatabase() {
-	dsn := "aprh8956_dbceria:dbceriangentot@tcp(103.247.8.36:3306)/aprh8956_dbceria?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+	dsn := "dbceria:dbceriasakti@tcp(db4free.net:3306)/dbceria?charset=utf8mb4&parseTime=True&loc=Local"
+	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
 		SingularTable: true,
 	}})
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Printf("Error")
 		return
 	}
-	DB = db
+	DB = conn
 }
- 
